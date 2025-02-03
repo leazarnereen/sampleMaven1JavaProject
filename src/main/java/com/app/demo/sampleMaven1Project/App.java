5ee1e9ace67283b4bb2e84ae088397e14a018237
leazarnereen <le123ar456@gmail.com>
2025-02-03 10:36:35 +0530

    Added all files

---
+++ src/main/java/com/app/demo/sampleMaven1Project/App.java
@@ -0,0 +1,13 @@
+package com.app.demo.sampleMaven1Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "I am Aiman!" );
+    }
+}
